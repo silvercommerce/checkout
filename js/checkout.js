631261ef7e314a88546b47a43a5f4ea290855e62
Morven Lewis-Everley <morven@i-lateral.com>
2018-02-16 17:00:03 +0000

    Disable required fields when deliver to this address is clicked

--- js/checkout.js
+++ js/checkout.js
@@ -2,8 +2,18 @@ var checkout_form = {
     delivery_check: null,
     delivery_fields: null,
     save_address: null,
+    required_fields: [
+        "CustomerDetailsForm_CustomerForm_ShippingAddress",
+        "CustomerDetailsForm_CustomerForm_DeliveryFirstName",
+        "CustomerDetailsForm_CustomerForm_DeliverySurname",
+        "CustomerDetailsForm_CustomerForm_DeliveryAddress1",
+        "CustomerDetailsForm_CustomerForm_DeliveryCity",
+        "CustomerDetailsForm_CustomerForm_DeliveryPostCode",
+        "CustomerDetailsForm_CustomerForm_DeliveryCountry"
+    ],
 
-    switchDelivery: function() {
+    // Show and hide delivery fields
+    switch_delivery: function() {
         if (this.delivery_check && this.delivery_check.checked == true) {
             if (this.delivery_fields) {
                 this.delivery_fields.style.display = "none";
@@ -20,6 +30,24 @@ var checkout_form = {
             }
         }
     },
+
+    // Disable and enable the required fields for shipping
+    switch_required: function() {
+        for (i = 0; i < this.required_fields.length; i++) {
+            var field = document.getElementById(this.required_fields[i]);
+
+            if (!field) {
+                continue;
+            }
+
+            if (this.delivery_check && this.delivery_check.checked == true) {
+                field.required = false;
+            } else {
+                field.required = true;
+            }
+        }
+    },
+    
     init: function() {
         this.delivery_check = document.getElementById("CustomerDetailsForm_CustomerForm_DuplicateDelivery");
         this.delivery_fields = document.getElementById("CustomerDetailsForm_CustomerForm_DeliveryFields_Holder");
@@ -29,10 +57,16 @@ var checkout_form = {
             var self = this;
             this.delivery_check.addEventListener(
                 "change",
-                this.switchDelivery.bind(this),
+                this.switch_delivery.bind(this),
                 false
             );
-            this.switchDelivery();
+            this.delivery_check.addEventListener(
+                "change",
+                this.switch_required.bind(this),
+                false
+            );
+            this.switch_delivery();
+            this.switch_required();
         }
     }
 }
