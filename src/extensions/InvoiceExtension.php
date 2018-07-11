4ec579f42abe9440e8fff33ece6832e0eb3f86bd
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-07-11 17:52:35 +0100

    Allow more complex editing/viewing of records

--- src/extensions/InvoiceExtension.php
+++ src/extensions/InvoiceExtension.php
@@ -4,8 +4,10 @@ namespace SilverCommerce\Checkout\Extensions;
 
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordViewer;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use SilverCommerce\OrdersAdmin\Forms\GridField\ReadOnlyGridField;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 
 class InvoiceExtension extends Extension
 {
@@ -17,10 +19,14 @@ class InvoiceExtension extends Extension
             'Root.Payments',
             ReadOnlyGridField::create(
                 "Payments",
-                "",
+                null,
                 $this->owner->Payments(),
-                $config = GridFieldConfig_RecordViewer::create()
+                $config = GridFieldConfig_RecordEditor::create()
             )
         );
+
+        $config
+            ->removeComponentsByType(GridFieldAddNewButton::class)
+            ->removeComponentsByType(GridFieldDeleteAction::class);
     }
 }
